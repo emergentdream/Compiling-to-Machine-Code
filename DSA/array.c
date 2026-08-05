f812c9f94b71175f3d4b231dc18526b85897d21a
emergentdream <311838756+emergentdream@users.noreply.github.com>
2026-08-04 21:14:05 -0400

    Try division,floats,array operations

---
+++ DSA/array.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int A[5] = {1,2,3};
+	printf("The third element is %d\n", A[4]);
+	return 0;
+}
---
+++ DSA/divide.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+double num1;
+
+int main() 
+{
+	num1 = 6.423e-4;
+	printf("division by 2 gives us %f", num1 / 2.0);
+    return 0;
+}
