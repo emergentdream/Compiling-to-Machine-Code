6d8aff9cda7fac73334c0c85c565ab10fe46985e
emergentdream <311838756+emergentdream@users.noreply.github.com>
2026-08-11 18:27:42 -0400

    checked whether empt arg list can be used to pass in args: can't

--- DSA/add.c
+++ DSA/add.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
+int checkvoid(char x);
 int add(int a, int b);
 int a,b,sum;
+char symbol;
+
 int main(void)
 {
 	printf("Enter first integer: ");
@@ -10,9 +13,17 @@ int main(void)
 	scanf("%d", &b);
 	sum = add(a,b);
 	printf("The sum is %d\n", sum);
+	symbol = 's';
+	checkvoid(symbol);
 }
 
 int add(int a, int b)
 {
 	return a + b;
+}
+
+int checkvoid(char x)
+{
+	printf("the symbol is %c\n", symbol);
+	return 0;
 }
